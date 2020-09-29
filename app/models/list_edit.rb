947fc72fe2e3e4a33874cc7bfa183fbba1bcf411
artwilton <Arthur.M.Wilton@gmail.com>
2020-09-29 19:37:18 -0400

    added multiple options for add and delete

--- app/models/list_edit.rb
+++ app/models/list_edit.rb
@@ -1,5 +1,4 @@
 module ListEdit
-    
 
     def search_movies
         puts "What movie are you looking for?"
@@ -8,34 +7,50 @@ module ListEdit
         result = JSON.parse(api_query)
         movies = result["Search"]
         
-        puts "We found #{movies.length} results:"
-        movies.each_with_index {|m,i| puts "#{i+1}. #{m['Title']} (#{m['Year']})"}
+        if movies == nil
+            puts "Sorry, we couldn't find any movies with that name."
+            self.main_menu
 
-        puts "What movie would you like to add to your collection? (press a number)"
-        input = gets.chomp
-        input = input.to_i
-        
-        if input <= movies.length
-            selection_id = movies[input - 1]["imdbID"]
-            movie_query = RestClient.get("https://www.omdbapi.com/", {params: {apikey: $apikey, i: selection_id}})
-            movie_result = JSON.parse(movie_query)
-            movie_to_add = movie_result
             
-            new_movie = Movie.find_or_create_by(imdb_id: movie_to_add["imdbID"]) do |film|
-                film.title = movie_to_add["Title"]
-                film.cast = movie_to_add["Actors"]
-                film.plot = movie_to_add["Plot"]
-                film.year = movie_to_add["Year"]
-                film.metascore = movie_to_add["Metascore"]
-                film.imdb_id = movie_to_add["imdbID"]
-            end
-            ListsMovies.find_or_create_by(movie_id: new_movie.id, list_id: self.list.id)
         else
-            puts "Invalid input!"
+            puts "We found #{movies.length} results:"
+
             movies.each_with_index {|m,i| puts "#{i+1}. #{m['Title']} (#{m['Year']})"}
+
+            puts "What movie(s) would you like to add to your collection?\n(enter one or more numbers)"
+            
+            multiple_inputs
+            input_check = @input.all? { |choice| choice.to_i <= movies.length }
+            
+            if input_check == true
+                @input.each do |i|
+                    @input = i.to_i
+                    selection_id = movies[@input - 1]["imdbID"]
+                    movie_query = RestClient.get("https://www.omdbapi.com/", {params: {apikey: $apikey, i: selection_id}})
+                    movie_result = JSON.parse(movie_query)
+                    movie_to_add = movie_result
+                    
+                    new_movie = Movie.find_or_create_by(imdb_id: movie_to_add["imdbID"]) do |film|
+                        film.title = movie_to_add["Title"]
+                        film.cast = movie_to_add["Actors"]
+                        film.plot = movie_to_add["Plot"]
+                        film.year = movie_to_add["Year"]
+                        film.metascore = movie_to_add["Metascore"]
+                        film.imdb_id = movie_to_add["imdbID"]
+                        film.director = movie_to_add["Director"]
+                    end
+                    ListsMovies.find_or_create_by(movie_id: new_movie.id, list_id: self.list.id)
+                end
+            else
+                puts "Invalid input!"
+                movies.each_with_index {|m,i| puts "#{i+1}. #{m['Title']} (#{m['Year']})"}
+            end
+
+            self.main_menu
+
         end
 
-        self.main_menu
+        
     end
 
     # can be more DRY here if we can call self.list_movies from movie_method.rb, but list_movies currently goes back to main menu
@@ -44,11 +59,10 @@ module ListEdit
         puts "~~~~" + self.list.name + "~~~~"
         #can add logic here to determine which list user wants to edit if we make users have more than one list
         movies = @session_user.movies.each_with_index {|movie, i| puts "#{i + 1}. #{movie.title}"}
-        puts movies
+        movies
         input = gets.chomp
-        input = input.to_i
 
-        if input <= movies.length
+        if input.to_i <= movies.length
             movie_to_delete_id = @session_user.movies[input - 1].id
             ListsMovies.where(list_id: @session_user.list.id, movie_id: movie_to_delete_id).destroy_all
         else
@@ -56,6 +70,13 @@ module ListEdit
         end
 
         self.main_menu
+    end
+
+    private
+
+    def multiple_inputs
+        @input = gets.chomp
+        @input = @input.split(%r{\s*,\s*|\s*|\s*\.\s*}).uniq
     end
     
 end
